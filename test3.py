a893e8cab646e0e37f4fd477612d250833b664fb
Andrey <andreyulianov@gmail.com>
2023-03-21 14:38:37 +1100

    test commit

---
+++ test3.py
@@ -0,0 +1,14 @@
+import time
+ 
+import oracledb
+import sample_env
+
+if not sample_env.get_is_thin():
+ oracledb.init_oracle_client(lib_dir=sample_env.get_oracle_client())
+print('1')
+
+connection = oracledb.connect(user=sample_env.get_main_user(),
+ password=sample_env.get_main_password(),
+ dsn=sample_env.get_connect_string())
+
+sprint('True')
